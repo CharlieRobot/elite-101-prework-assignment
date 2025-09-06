8a175ecc719369b159db9c705ee5ef83e9719adf
CharlieRobot <robotpfertner@gmail.com>
2025-09-06 18:36:06 +0000

    Added placeholder options and a goodbye message.

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-print("Welcome to the Github chatbot")
-name = input("What is your name? ")
-age = int(input("hello "+ name + ", how old are you? "))
+print("Welcome to the tech support chatbot")
+name = input("Please enter your name: ")
+age = int(input("Hello "+ name + ", how old are you? "))
+
+print("Here are the following options you can choose from:\n1. \n2.\n3. \n4. \n5. Exit the conversation.")
+the_choice = int(input("Enter the number of your choice: "))
+if the_choice == 5:
+    print("Thank you for using the chatbot, "+name+". Have a great rest of your day!")
+    exit()
